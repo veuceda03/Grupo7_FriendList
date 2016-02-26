9660a21e50e031d5dd53d9bdf6913ae6a4abcebc
viancamadrid <viancamadrid@unitec.edu>
2016-02-26 15:41:17 -0600

    opcion

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <ncurses.h>
 
+using std::cin;
+using std:cout;
+using std::endl;
+
+int menu();
 
 int main(int argc,char*argv[]){
 
 	return 0;
+}
+
+int menu(){
+	int opcion;
+	cout<<".-*.-*.-MENU-.*.-*."<<endl;
+	cout<<"1.-) Agregar Contacto\n2.)Listar Contacto\n3.)Salir\n";
+	cin>>opcion;
+	return opcion;
 }
